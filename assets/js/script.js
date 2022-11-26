ed05feef3c9db388fb415922e5bd112ceea5e193
cridder <cjridder@gmail.com>
2022-11-26 13:02:46 -0800

    working example

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@
 // var json = JSON.stringify(obj);
 // console.log(json);
 
-
+// get city list data from json file
 var request = new XMLHttpRequest();
 request.open("GET", "../assets/json/city.list.json", false);
 request.send(null);
@@ -33,3 +33,45 @@ request.onreadystatechange = function () {
     console.log(my_JSON_object);
   }
 };
+
+// 
+
+const search = document.getElementById('search')
+const matchList = document.getElementById('match-list')
+
+// search json file and filter it
+const searchStates = async (searchText) => {
+    const res = await fetch("../assets/json/state_capitals.json");
+    const states = await res.json();
+
+  // get matches to current text input
+  let matches = states.filter(state => {
+    const regex = new RegExp(`^${searchText}`, 'gi');
+    return state.name.match(regex)  || state.abbr.match(regex);
+  });
+
+  if (searchText.length === 0) {
+    matches = [];
+    matchList.innerHTML = '';
+  }
+console.log(matches);
+outputHtml(matches);
+
+};
+
+//  show results in HTML
+  const outputHtml = matches => {
+    if(matches.length > 0) {
+      const html = matches.map(match => `
+        <div class="card card-body mb-1">
+            <h4>${match.name} (${match.abbr}) <span class="text-primary">${match.capital}</span></h4>
+            <small>lat: ${match.lat} / long: ${match.long}</small>
+        </div>
+        `
+      ).join('');
+      console.log(html);
+    // matchList.innerHTML = html;
+    }
+  };
+
+search.addEventListener('input', () => searchStates(search.value));
